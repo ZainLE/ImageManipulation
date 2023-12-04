f44bf1a8769f19b8c34b6a6f0dfb5fac9f04c2b2
ZainLatif7164 <latifzain40@gmail.com>
2023-12-04 20:16:19 +0100

    6 classes with different filters

--- imageM.py
+++ imageM.py
@@ -3,7 +3,10 @@ import pygame
 # initialize pygame and screen
 pygame.init()
 screen = pygame.display.set_mode((1200, 400))
-    
+
+red_input=float(input("Write the value of red between (0,255):  "))
+green_input=float(input("Write the value of green between (0,255):  "))
+blue_input=float(input("Write the value of blue between (0,255):  "))
 
 class Filter():
     def __init__(self,image_file):
@@ -16,8 +19,7 @@ class Filter():
         self.arr = pygame.surfarray.array3d(self.img)
         # print(self.arr.shape)
         # print(self.arr)
-        
-        
+            
     def save_image(self, path):
         surface = pygame.surfarray.make_surface(self.arr) # convert array back to pygame surface
         pygame.image.save(surface, path) # save the surface as png
@@ -34,28 +36,88 @@ class GrayScaleTransformer(Filter):
                 self.arr[i,j] = [avg,avg,avg]
 
 class MyFunnyFilter(Filter):
-    def __init__(self, image_file2):
-        super().__init__(image_file2)
+    def __init__(self, image_file):
+        super().__init__(image_file)
         
     def transform(self):
         for i in range(self.arr.shape[0]):
             for j in range(self.arr.shape[1]):
                 r,g,b = self.arr[i][j]
-                self.arr[i][j]=[b,g,r]
-        
+                self.arr[i][j]=[g,b,r]
+
+class MyblackFilter(Filter):
+    def __init__(self, image_file):
+        super().__init__(image_file)
+    
+    def transform(self):
+        for i in range(self.arr.shape[0]):
+            for j in range(self.arr.shape[1]):
+                self.arr[i][j]  =[0,0,0]
+
+
+
+class MyWhiteFilter(Filter):
+    def __init__(self, image_file):
+        super().__init__(image_file)
+    
+    def transform(self):
+        for i in range(self.arr.shape[0]):
+            for j in range(self.arr.shape[1]):
+                self.arr[i][j]  =[255,255,255]
+                
+                
+                
+class User_Filter(Filter):
+    def __init__(self, image_file):
+        super().__init__(image_file)
+    
+    def transform(self):
+        for i in range(self.arr.shape[0]):
+            for j in range(self.arr.shape[1]):
+                self.arr[i][j]  =[red_input,green_input,blue_input]
+
+
+
     
 
-image_file = "data/test_gray.png"
-image_file2= "data/test_cut.png"
-transformer = GrayScaleTransformer(image_file)
+image_file = "data/test_cut.png"
+# image_file2= "data/test_gray.png"
+# image_file3= "data/test_black.png"
+# image_file4= "data/test_white.png"
+# image_file5= "data/test_input.png"
 
+#Gray 
+transformer = GrayScaleTransformer(image_file)
 transformer.load_image()
 transformer.transform()
 transformer.save_image("data/test_gray.png")
 
-transformer = MyFunnyFilter(image_file2)
-
-
+#Funny
+transformer = MyFunnyFilter(image_file)
 transformer.load_image()
 transformer.transform()
 transformer.save_image("data/test_funny.png")
+
+
+# Black 
+transformer = MyblackFilter(image_file)
+transformer.load_image()
+transformer.transform()
+transformer.save_image("data/test_black.png")
+
+
+#White 
+transformer = MyWhiteFilter(image_file)
+transformer.load_image()
+transformer.transform()
+transformer.save_image("data/test_white.png")
+
+
+# User Input
+transformer = User_Filter(image_file)
+transformer.load_image()
+transformer.transform()
+transformer.save_image("data/test_input.png")
+
+
+
