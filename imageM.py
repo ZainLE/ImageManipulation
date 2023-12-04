fc3f52623ade0b14250c5b715e458c9980b09979
ZainLatif7164 <latifzain40@gmail.com>
2023-12-04 16:01:07 +0100

    Changes Done

--- imageM.py
+++ imageM.py
@@ -3,25 +3,59 @@ import pygame
 # initialize pygame and screen
 pygame.init()
 screen = pygame.display.set_mode((1200, 400))
+    
 
-# load the image and convert to numpy array
-img = pygame.image.load('data/test_cut.png').convert_alpha()
-arr = pygame.surfarray.array3d(img)
+class Filter():
+    def __init__(self,image_file):
+        self.image_file = image_file
+        self.img = None
+        self.arr = None
+        
+    def load_image(self):
+        self.img = pygame.image.load(self.image_file).convert_alpha()
+        self.arr = pygame.surfarray.array3d(self.img)
+        # print(self.arr.shape)
+        # print(self.arr)
+        
+        
+    def save_image(self, path):
+        surface = pygame.surfarray.make_surface(self.arr) # convert array back to pygame surface
+        pygame.image.save(surface, path) # save the surface as png
+        
+class GrayScaleTransformer(Filter):
+    def __init__(self, image_file):
+        super().__init__(image_file)
+        
+    def transform(self):
+        for i in range(self.arr.shape[0]):
+            for j in range(self.arr.shape[1]):
+                r,g,b = self.arr[i][j]
+                avg = r/3 + g/3 + b/3
+                self.arr[i,j] = [avg,avg,avg]
 
-# print the shape of the array
-print(arr.shape)
+class MyFunnyFilter(Filter):
+    def __init__(self, image_file2):
+        super().__init__(image_file2)
+        
+    def transform(self):
+        for i in range(self.arr.shape[0]):
+            for j in range(self.arr.shape[1]):
+                r,g,b = self.arr[i][j]
+                self.arr[i][j]=[b,g,r]
+        
+    
 
-# print couple of pixels to understand the structure
-print(arr[2][0])
-print(arr[3][0])
-print(arr[4][0])
+image_file = "data/test_gray.png"
+image_file2= "data/test_cut.png"
+transformer = GrayScaleTransformer(image_file)
 
-# convert to grayscale using for loops
-for i in range(0, 16): # column number from 0 to 15
-    for j in range(0, 16): # row number from 0 to 15
-        r, g, b = arr[i][j] # get rgb values for the pixel
-        avg = r/3  + g/3  + b/3 # calculate average
-        arr[i][j] = [avg, avg, avg] # set rgb values to average
+transformer.load_image()
+transformer.transform()
+transformer.save_image("data/test_gray.png")
 
-surface = pygame.surfarray.make_surface(arr) # convert array back to pygame surface
-pygame.image.save(surface, 'data/test_gray.png') # save the surface as png
+transformer = MyFunnyFilter(image_file2)
+
+
+transformer.load_image()
+transformer.transform()
+transformer.save_image("data/test_funny.png")
